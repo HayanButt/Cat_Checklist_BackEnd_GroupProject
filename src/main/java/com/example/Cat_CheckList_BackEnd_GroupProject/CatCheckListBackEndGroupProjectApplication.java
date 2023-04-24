19cc2e6c6ede206417ed6e7bb314606995db5c57
Yasmin haidar <yasmin1635@hotmail.com>
2023-04-24 11:07:23 +0100

    initial commit

---
+++ src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/CatCheckListBackEndGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cat_CheckList_BackEnd_GroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatCheckListBackEndGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatCheckListBackEndGroupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cat_CheckList_BackEnd_GroupProject/CatCheckListBackEndGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cat_CheckList_BackEnd_GroupProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatCheckListBackEndGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
