e539bc38ade20760e9e4af4e1250eef0363be260
Yasmin haidar <yasmin1635@hotmail.com>
2023-04-25 09:39:13 +0100

    Cat model completed

--- src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/models/Cat.java
+++ src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/models/Cat.java
@@ -1,4 +1,87 @@
 package com.example.Cat_CheckList_BackEnd_GroupProject.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity(name = "cat")
+
 public class Cat {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private long id;
+
+    @Column
+    private String name;
+    @Column
+    private String birthday;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties({"cats"})
+    private User user;
+    @Column
+    private List<Task> tasks;
+
+
+    public Cat( User user, long id, String name, String birthday){
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.tasks = new ArrayList<>();
+        this.user = user;
+
+    }
+
+
+    public Cat(){
+
+    }
+
+//    GETTERS & SETTERS
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
