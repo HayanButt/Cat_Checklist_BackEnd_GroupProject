e0d7721446abc863243bb086926138b82475ce51
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-04-25 14:57:39 +0100

    finished catcontroller and catservices

--- src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/controllers/CatController.java
+++ src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/controllers/CatController.java
@@ -5,9 +5,7 @@ import com.example.Cat_CheckList_BackEnd_GroupProject.services.CatServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,22 @@ public class CatController {
     @GetMapping
     public ResponseEntity<List<Cat>> getAllCats(){
         return new ResponseEntity<>(catServices.getAllCats(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cat> getCatById(@PathVariable Long id){
+        return new ResponseEntity<>(catServices.getCatById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<Cat> addNewCat(@PathVariable Long id, @RequestBody Cat cat){
+        return new ResponseEntity<>(catServices.saveNewCat(cat,id), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteCat(@PathVariable Long id){
+        catServices.deleteCat(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/services/CatServices.java
+++ src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/services/CatServices.java
@@ -3,6 +3,7 @@ package com.example.Cat_CheckList_BackEnd_GroupProject.services;
 import com.example.Cat_CheckList_BackEnd_GroupProject.models.Cat;
 import com.example.Cat_CheckList_BackEnd_GroupProject.models.User;
 import com.example.Cat_CheckList_BackEnd_GroupProject.repositories.CatRepository;
+import com.example.Cat_CheckList_BackEnd_GroupProject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,8 @@ import java.util.List;
 public class CatServices {
     @Autowired
     CatRepository catRepository;
+    @Autowired
+    UserRepository userRepository;
     public void addNewCat(Cat cat){
         catRepository.save(cat);
     }
@@ -22,5 +25,20 @@ public class CatServices {
     public List<Cat> getAllCats(){
         return catRepository.findAll();
 
+    }
+    public Cat getCatById(Long id) {
+        return catRepository.findById(id).get();
+
+    }
+
+    public Cat saveNewCat(Cat cat, long id) {
+       User user = userRepository.findById(id).get();
+       cat = new Cat(user,cat.getName(),cat.getBirthday());
+       return catRepository.save(cat);
+    }
+
+
+    public void deleteCat(Long id) {
+        catRepository.deleteById(id);
     }
 }
