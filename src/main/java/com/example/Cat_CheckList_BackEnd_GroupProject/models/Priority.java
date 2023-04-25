a4a2027f9130cdc8eec23af580087b570d2df86b
Yasmin haidar <yasmin1635@hotmail.com>
2023-04-25 10:08:32 +0100

    Priority Enum created

--- src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/models/Priority.java
+++ src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/models/Priority.java
@@ -1,4 +1,21 @@
 package com.example.Cat_CheckList_BackEnd_GroupProject.models;
 
 public enum Priority {
+
+    LOW("LOW"),
+    MEDIUM("MEDIUM"),
+    HIGH("HIGH");
+
+    private String abbreviation;
+
+    Priority(String abbreviation){
+        this.abbreviation = abbreviation;
+    }
+
+    public String getAbbreviation(){
+        return this.abbreviation;
+    }
+
+
+
 }
