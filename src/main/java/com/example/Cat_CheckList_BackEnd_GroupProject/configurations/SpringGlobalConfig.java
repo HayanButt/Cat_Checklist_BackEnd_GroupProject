9e7e19c296f325cf8024d1c09d3384170e86947b
Hayan Butt <hayan_880@hotmail.com>
2023-05-26 11:10:24 +0100

    first fetch request

---
+++ src/main/java/com/example/Cat_CheckList_BackEnd_GroupProject/configurations/SpringGlobalConfig.java
@@ -0,0 +1,17 @@
+package com.example.Cat_CheckList_BackEnd_GroupProject.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings (CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
